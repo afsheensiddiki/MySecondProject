c043937c42d8dad2b368fbe7819da4ca317ddbd2
dilmuratd <dilmuratd@gmail.com>
2018-03-10 14:46:56 -0500

    initialize commit

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
 	
 	System.out.println("my name is Afsheeno");
 	System.out.println("hahahaha");
+System.out.println("Aapke kya haal hain?");
+System.out.println("Kya ho raha hai?");
 }
 
 }
